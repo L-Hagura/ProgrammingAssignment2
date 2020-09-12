c0778c241bac8eb3f6c35778186076b4565fd8e4
L-Hagura <larynda.hagura@gmail.com>
2020-09-11 22:17:14 -0700

    Upload cachematrix.R for R-programming course Wk3 homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This mini program returns the inverse of a matrix
+##If the inverse already exists, then it will be returned
+##without further calculation
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  iMatrix <- NULL
+  set <- function(y) {##Reset input and inversed matrix
+    x <<- y
+    iMatrix <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setInverse <- function(inverse) {
+    iMatrix <<- inverse
+  }
+  getInverse <- function() {
+    iMatrix
+  }
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        iMatrix <- x$getInverse()
+        if (!is.null(iMatrix)) {
+          massage("cached data is returned")
+          return(iMatrix)
+        }
+        data <- x$get()
+        iMatrix <- solve(data, ...)
+        x$setInverse(iMatrix)
+        iMatrix
 }
+
